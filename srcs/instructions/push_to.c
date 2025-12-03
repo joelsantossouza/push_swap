e760807e00337dd24813e3e8fc7f27a9d97601ec
joel <joelsantossouza08@gmail.com>
2025-12-03 20:14:10 +0000

    Fix some motions bugs

--- srcs/instructions/push_to.c
+++ srcs/instructions/push_to.c
@@ -6,7 +6,7 @@
 /*   By: joesanto <joesanto@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/03 18:33:06 by joesanto          #+#    #+#             */
-/*   Updated: 2025/12/03 19:20:30 by joesanto         ###   ########.fr       */
+/*   Updated: 2025/12/03 20:13:31 by joesanto         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,20 +15,20 @@
 
 void	push_to_a(size_t *size_a, int stack_a[*size_a], size_t *size_b, int stack_b[*size_b])
 {
-	if (!size_b)
+	if (!*size_b)
 		return ;
-	ft_memmove(stack_a + 1, stack_a, (*size_a)++);
+	ft_memmove(stack_a + 1, stack_a, sizeof(int) * (*size_a)++);
 	stack_a[0] = stack_b[0];
-	ft_memmove(stack_b, stack_b + 1, --(*size_b));
+	ft_memmove(stack_b, stack_b + 1, sizeof(int) * --(*size_b));
 	write(STDOUT_FILENO, "pa\n", 3);
 }
 
 void	push_to_b(size_t *size_b, int stack_b[*size_b], size_t *size_a, int stack_a[*size_a])
 {
-	if (!size_a)
+	if (!*size_a)
 		return ;
-	ft_memmove(stack_b + 1, stack_b, (*size_b)++);
+	ft_memmove(stack_b + 1, stack_b, sizeof(int) * (*size_b)++);
 	stack_b[0] = stack_a[0];
-	ft_memmove(stack_a, stack_a + 1, --(*size_a));
+	ft_memmove(stack_a, stack_a + 1, sizeof(int) * --(*size_a));
 	write(STDOUT_FILENO, "pb\n", 3);
 }
