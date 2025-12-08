eb6f7458e9ee114845a4d84cf6360b5e31740bdb
joel <joelsantossouza08@gmail.com>
2025-12-08 00:21:38 +0000

    Optimizing push_swap

--- srcs/stack_insertion/is_stack_sorted.c
+++ srcs/stack_insertion/is_stack_sorted.c
@@ -6,7 +6,7 @@
 /*   By: joesanto <joesanto@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/08 00:03:32 by joesanto          #+#    #+#             */
-/*   Updated: 2025/12/08 00:10:59 by joesanto         ###   ########.fr       */
+/*   Updated: 2025/12/08 00:19:27 by joesanto         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,18 +18,19 @@ int	is_stack_sorted(t_stack *stack)
 	const size_t	size = stack->size;
 	size_t			start;
 	size_t			i;
-	size_t			next;
+	size_t			prev;
 
 	if (stack->size < 2)
 		return (1);
 	start = get_min(stack->data, stack->size);
-	i = start;
+	prev = start;
+	i = (start + 1) % size;
 	while (i != start)
 	{
-		next = (i + 1) % size;
-		if (data[i] > data[next])
+		if (data[prev] > data[i])
 			return (0);
-		i = next;
+		prev = i;
+		i = (i + 1) % size;
 	}
 	return (1);
 }
