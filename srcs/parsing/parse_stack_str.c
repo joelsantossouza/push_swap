fe2789c957740d93c524f05584daa3f8367b6231
joel <joelsantossouza08@gmail.com>
2025-12-08 01:43:33 +0000

    Update of ft_split

--- srcs/parsing/parse_stack_str.c
+++ srcs/parsing/parse_stack_str.c
@@ -6,7 +6,7 @@
 /*   By: joesanto <joesanto@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/05 15:54:10 by joesanto          #+#    #+#             */
-/*   Updated: 2025/12/05 16:31:51 by joesanto         ###   ########.fr       */
+/*   Updated: 2025/12/08 01:42:47 by joesanto         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,12 +16,15 @@
 
 int	parse_stack_str(t_stack *stack, char *numbers)
 {
-	const size_t	size = ft_word_count(numbers, ' ');
-	char			**splited;
-	int				stats;
+	ssize_t	size;
+	char	**splited;
+	int		stats;
 
-	splited = ft_split(numbers, ' ');
-	if (!splited)
+	if (!*numbers)
+		return (error(), -1);
+	splited = 0;
+	size = ft_split(&splited, numbers, ' ');
+	if (size < 0)
 		return (-1);
 	stats = parse_stack_lst(stack, size, splited);
 	ft_freearray((void **) splited, free);
